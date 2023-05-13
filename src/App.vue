977d8903f8701bd110ec51f8516a4b2dd5ae7356
sanchoouse <sanchoouse@mail.ru>
2023-05-13 22:03:34 +0700

    Kinopoisk API + Electron on Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="text-center mt-5">КИНОПОИСК🍿</h2>
+        <p>Следите за самыми популярными мультфильмами и аниме, которые в тренде.</p>
+      </div>
+
+      <div class="my-4">
+        <a href="#" @click="getTrendingMovies('3')" class="mx-3 h4">
+          Мультфильмы</a
+        >
+        <a href="#" @click="getTrendingMovies('4')" class="mx-3 h4"
+          >Аниме</a
+        >
+      </div>
+
+      <div class="row" v-if="movies.length > 0">
+        <div class="col-md-3" v-for="(movie, i) in movies" :key="i">
+          <movie-card :movie="movie" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MovieCard from "./components/MovieCard.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MovieCard,
+  },
+  data() {
+    return {
+      movies: [],
+      apiKey: "PA12R92-7HTM5N7-Q9W7CRH-F2JRYF8",
+    };
+  },
+  methods: {
+    getTrendingMovies(category) {
+      return fetch(
+        `https://api.kinopoisk.dev/v1.3/movie?page=1&limit=10&typeNumber=${category}`,
+        {
+          headers: {
+            "X-API-KEY": "PA12R92-7HTM5N7-Q9W7CRH-F2JRYF8"
+          }
+        }
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+          }
+          console.log(response);
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data.docs);
+          this.movies = data.docs;
+        });
+    },
+  },
+  mounted() {
+    this.getTrendingMovies("3");
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <main>
+      <div class="card mb-4 shadow-lg border border-secondary bg-dark text-light">
+        <img
+          class="card-img-top"
+          :src="`${movie.poster.url}`"
+          alt="Card image cap"
+        />
+        <div class="card-body overflow-auto"  style="height: 300px;">
+          <p class="card-text">{{ movie.name }}</p>
+          <div class="">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  <i class="fas fa-heart"></i>
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  <i class="fas fa-plus"></i>
+                </button>
+              </div>
+              <small class="ms-auto text-muted"
+                >{{movie.description}}</small
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </template>
+
+  <script>
+  export default {
+    name: "MovieCard",
+    props: {
+      movie: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+  </script>
